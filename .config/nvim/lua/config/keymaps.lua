2d94b7c60c456478d01222bbd813ead966ad3488
alex-bezverkhniy <alexandr.bezverkhniy@gmail.com>
2024-08-09 16:01:13 -0500

    added custom DAP config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+map("i", "<A-Right>", "<esc>A", { desc = "End of the line" })
+map("i", "<A-Left>", "<esc>I", { desc = "Start of the line" })
+
+map("n", "<A-Left>", "<C-Left>", { desc = "Decrease windows width" })
+map("n", "<A-Right>", "<C-Right>", { desc = "Increase windows width" })
+
+map("n", "<A-Up>", "<C-Up>", { desc = "Increase windows height" })
+map("n", "<A-Down>", "<C-Down>", { desc = "Decrease windows height" })
